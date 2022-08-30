b10b3f20484c86a11f2741a76850856de038d364
Krzysztof Brzozowski <krzysztofbrzozowski7@gmail.com>
2022-08-30 23:09:41 +0100

    mobile version navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import styled from "styled-components"
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Wrapper>
+      <Navbar/>
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+display:flex;
+flex-direction:row;
+width:100vw;
+height:100vh;
+padding:1rem 2rem;
+`
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import styled from "styled-components"
+import {ReactComponent as Logo} from "../images/logo.svg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar() {
   return (
-    <header>
-        <nav>
-
-        </nav>
-    </header>
+    <Header>
+      <Logo/>
+        <NavbarContainer>
+        <FontAwesomeIcon icon={faBars} />
+        </NavbarContainer>
+    </Header>
   )
 }
+const Header = styled.header`
+display:flex;
+flex-direction:row;
+width:100%;
+justify-content:space-between;
+`
+const NavbarContainer = styled.nav`
+
+`
